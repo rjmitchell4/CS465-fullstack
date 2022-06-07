b14ee55e18372fcb9b1a9735204007f945ca7a5f
Ryan Mitchell <ryan.mitchell4@snhu.edu>
2022-06-06 17:13:16 -0700

    RESTful API

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const hbs = require('hbs');
+require('./app_api/database/db');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
+const apiRouter = require('./app_api/routes/index');
 
 const app = express();
-const hbs = require('hbs');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
@@ -28,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
